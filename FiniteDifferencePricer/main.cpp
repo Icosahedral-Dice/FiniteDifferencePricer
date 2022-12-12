751c231be7a09f3f8dfc060aab206c1557aea960
Mingsen Wang <mingsen.wang.sh@gmail.com>
2022-12-11 22:06:30 -0500

    Initial Commit

---
+++ FiniteDifferencePricer/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  FiniteDifferencePricer
+//
+//  Created by 王明森 on 12/11/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
